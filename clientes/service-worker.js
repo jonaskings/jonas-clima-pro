f5457ec7efb97dccf6270cb83e6e8f07c874cdf2
Jonas Clima Pro <jonaskings@icloud.com>
2026-08-27 20:31:31 +0200

    Force PWA updates for both apps

--- clientes/service-worker.js
+++ clientes/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = 'jonas-clima-clientes-v5';
+const CACHE = 'jonas-clima-clientes-v6';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './firebase-config.js', '../jonas-clima-pro-icon.png'];
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))));
--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = 'jonas-clima-pro-v30';
+const CACHE = 'jonas-clima-pro-v31';
 const ASSETS = ['./', './index.html', './vip.html', './manifest.webmanifest', './jonas-clima-pro-icon.png', './firebase-config.js'];
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))));
