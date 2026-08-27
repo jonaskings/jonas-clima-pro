f83330a93884b7dcbc83aead3fea36d3d0fd21a5
Jonas Clima Pro <jonaskings@icloud.com>
2026-08-28 00:58:41 +0200

    Refresh orders when app resumes

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,11 @@
-const CACHE = 'jonas-clima-pro-v36';
+const CACHE = 'jonas-clima-pro-v37';
 const ASSETS = ['./', './index.html', './vip.html', './manifest.webmanifest', './jonas-clima-pro-icon.png', './firebase-config.js'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
-self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))));
+self.addEventListener('fetch', event => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(fetch(event.request).then(response => { const copy = response.clone(); caches.open(CACHE).then(cache => cache.put('./index.html', copy)); return response; }).catch(() => caches.match('./index.html')));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request)));
+});
