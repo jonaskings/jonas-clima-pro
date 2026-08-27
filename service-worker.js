51a719d223feeb8a525db47bda0044376934719e
jonaskings <jonaskings@icloud.com>
2026-08-27 02:15:10 +0200

    Actualizar caché de la aplicación

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,6 @@
-const CACHE = 'jonas-clima-pro-v1';
+const CACHE = 'jonas-clima-pro-v2';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
 self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))));
+
