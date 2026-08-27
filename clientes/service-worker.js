ab3588f615f4887e0e3a5296dae9836505cebfe1
Jonas Clima Pro <jonaskings@icloud.com>
2026-08-27 20:45:01 +0200

    Collect customer address and housing details

--- clientes/service-worker.js
+++ clientes/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'jonas-clima-clientes-v6';
+const CACHE = 'jonas-clima-clientes-v7';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './firebase-config.js', '../jonas-clima-pro-icon.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
