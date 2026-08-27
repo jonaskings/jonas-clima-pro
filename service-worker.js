771c7dc88e3db54fc41aa3a5c9e44a1fb17a7ec3
jonaskings <jonaskings@icloud.com>
2026-08-27 02:44:19 +0200

    Actualizar caché para nueva versión

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = 'jonas-clima-pro-v2';
-const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
+const CACHE = 'jonas-clima-pro-v7';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg', './jonas-clima-pro-logo.jpeg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
 self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))));
