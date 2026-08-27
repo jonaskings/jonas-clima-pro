2c6bdcbdcf7955f1a9ac40a27cb022ab3859a616
jonaskings <jonaskings@icloud.com>
2026-08-27 16:15:09 +0200

    Actualizar caché del icono

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'jonas-clima-pro-v8';
+const CACHE = 'jonas-clima-pro-v9';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg', './jonas-clima-pro-logo.jpeg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
