9def1306d558fbad42b421b35f5c7c20288508f9
Jonas Clima Pro <jonaskings@icloud.com>
2026-08-28 07:44:24 +0200

    Sync client invoices and claims

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'jonas-clima-pro-v38';
+const CACHE = 'jonas-clima-pro-v39';
 const ASSETS = ['./', './index.html', './vip.html', './manifest.webmanifest', './jonas-clima-pro-icon.png', './firebase-config.js'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
